36652263a7ed9db23ae98214534a5ba8b9170054
smuvvads <sxmuvva1@shockers.wichita.edu>
2022-12-03 20:49:00 -0600

    Backend and front end added, still need some Query changes in front end

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask, flash, render_template, redirect, url_for, request, session, jsonify
+from module.database import Database
+import datetime
+
+
+app = Flask(__name__)
+app.secret_key = "mySc@$1emp"
+db = Database()
+
+@app.route('/')
+def index():
+    response = {}
+    response['employees']= db.get_all_employees(None)
+    response['dept'] = db.get_deparments_list()
+    print(response['dept'])
+    return render_template('index.html', data= response)
+
+@app.route('/create',  methods=['POST', 'GET'])
+def createEmployee():
+    if request.method == 'POST' and request.form['save']:
+        emp_data = {"first_name": request.form['first_name'],
+                    "last_name": request.form['last_name'],
+                    "personal_email_id": request.form['personal_email_id'],
+                    "phone_number": request.form['phone_number']
+                    }
+        emp_id = db.insert(emp_data)
+        if emp_id:
+            dept_emp = {'emp_no':emp_id,
+                        "dept_no":request.form['dept_no'],
+                        "from_date": datetime.datetime.now(),
+                        "to_date":datetime.datetime.now(),
+                        "is_active":1}
+            salary_det = {'emp_id':emp_id,
+                          "salary": request.form['salary'],
+                          "from_date": datetime.datetime.now(),
+                          "to_date":datetime.datetime.now()}
+            db.insert_dept_emp(dept_emp)
+            db.insert_salary_emp(salary_det)
+            flash("A new employee has been added")
+            return redirect(url_for('index'))
+        else:
+            flash("A new employee can not be added")
+            return redirect(url_for('index'))
+    else:
+        flash("Please enter proper details")
+        return redirect(url_for('index'))
+
+@app.route('/loadempdata', methods=['POST', 'GET'])
+def ajax_load_emp_data():
+    response = {}
+    emp_id = request.form['emp_id']
+    response['employees'] = db.get_all_employee_details_by_id(emp_id)
+    response['dept'] = db.get_deparments_list()
+    return jsonify({'htmlresponse': render_template('emp_load_ajax.html', data= response)})
+
+@app.route('/update', methods=['POST', 'GET'])
+def update_emp_data():
+    if request.method == 'POST' and request.form['update']:
+        emp_data = {"first_name": request.form['first_name'],
+                    "last_name": request.form['last_name'],
+                    "personal_email_id": request.form['personal_email_id'],
+                    "phone_number": request.form['phone_number']
+                    }
+        emp_id = request.form['emp_id']
+        if db.emp_detail_update(emp_id, emp_data):
+            dept_emp = {'emp_no':emp_id,
+                        "dept_no":request.form['dept_no'],
+                        }
+            salary_det = {'emp_id':emp_id,
+                          "salary": request.form['salary'],
+                          }
+            db.emp_slaray_update(emp_id, salary_det)
+            db.emp_dept_update(emp_id, dept_emp)
+            flash("employee detail has been updated")
+            return redirect(url_for('index'))
+        else:
+            flash("employee detail can not be updated")
+            return redirect(url_for('index'))
+    else:
+        flash("Please enter proper details")
+        return redirect(url_for('index'))
+
+@app.route('/delete_emp', methods=['POST', 'GET'])
+def delete_emp():
+    emp_id = request.form['emp_id']
+    if(db.delete(emp_id)):
+        return 'Success'
+    else:
+        return 'Error'
+
+if __name__ == '__main__':
+    app.run()
---
+++ module/database.py
@@ -0,0 +1,160 @@
+import pymysql
+
+class Database:
+    def connect(self):
+        return pymysql.connect(host="localhost", user="root", password="g453u794", database="employeemanagement",
+                               charset='utf8mb4')
+
+    def insert(self, data):
+        con = Database.connect(self)
+        cursor = con.cursor()
+
+        try:
+            cursor.execute("INSERT INTO employees(first_name,last_name,personal_email_id, phone_number) VALUES(%s, %s, %s, %s)",
+                           (data['first_name'], data['last_name'], data['personal_email_id'],data['phone_number'],))
+            con.commit()
+
+            return cursor.lastrowid
+        except:
+            con.rollback()
+            return None
+        finally:
+            con.close()
+
+    def insert_dept_emp(self,data):
+        con = Database.connect(self)
+        cursor = con.cursor()
+        try:
+            cursor.execute(
+                "INSERT INTO dept_emp(emp_no, dept_no, from_date, to_date, is_active) VALUES(%s, %s, %s, %s, %s)",
+                (data['emp_no'], data['dept_no'], data['from_date'], data['to_date'], data['is_active']))
+            con.commit()
+            return cursor.lastrowid
+        except:
+            con.rollback()
+            return None
+        finally:
+            con.close()
+
+    def insert_salary_emp(self, data):
+        con = Database.connect(self)
+        cursor = con.cursor()
+        try:
+            cursor.execute(
+                "INSERT INTO salaries(emp_id,salary,from_date, to_date) VALUES(%s, %s, %s, %s)",
+                (data['emp_id'], data['salary'], data['from_date'], data['to_date'],))
+            con.commit()
+            return cursor.lastrowid
+        except:
+            con.rollback()
+            return None
+        finally:
+            con.close()
+
+    def emp_detail_update(self, id, data):
+        con = Database.connect(self)
+        cursor = con.cursor()
+        try:
+            cursor.execute("UPDATE employees set first_name = %s, last_name = %s, personal_email_id = %s, phone_number=%s where emp_id = %s",
+                           (data['first_name'], data['last_name'], data['personal_email_id'],data['phone_number'], id,))
+            con.commit()
+
+            return True
+        except:
+            con.rollback()
+            return False
+        finally:
+            con.close()
+
+    def emp_slaray_update(self, id, data):
+        con = Database.connect(self)
+        cursor = con.cursor()
+        try:
+            cursor.execute("UPDATE salaries set salary = %s where emp_id = %s",
+                           (data['salary'], id,))
+            con.commit()
+
+            return True
+        except:
+            con.rollback()
+            return False
+        finally:
+            con.close()
+
+    def emp_dept_update(self, id, data):
+        con = Database.connect(self)
+        cursor = con.cursor()
+        try:
+            cursor.execute("UPDATE dept_emp set dept_no = %s where emp_no = %s",
+                           (data['dept_no'], id,))
+            con.commit()
+
+            return True
+        except:
+            con.rollback()
+            return False
+        finally:
+            con.close()
+
+    def delete(self, id):
+        con = Database.connect(self)
+        cursor = con.cursor()
+        try:
+            cursor.execute("DELETE FROM salaries where emp_id = %s", (id,))
+            cursor.execute("DELETE FROM dept_emp where emp_no = %s", (id,))
+            cursor.execute("DELETE FROM employees where emp_id = %s", (id,))
+            con.commit()
+            return True
+        except:
+            con.rollback()
+            return False
+        finally:
+            con.close()
+
+    def get_all_employees(self, id=None):
+        con = Database.connect(self)
+        cursor = con.cursor()
+
+        try:
+            if id == None:
+                sql = "SELECT * FROM employees AS e JOIN salaries AS s ON e.emp_id = s.emp_id" \
+                      " JOIN dept_emp as de ON e.emp_id = de.emp_no" \
+                      " JOIN deparments as d ON de.dept_no= d.dep_no order by e.emp_id DESC"
+                print(sql)
+                cursor.execute(sql)
+            else:
+                cursor.execute(
+                    "SELECT * FROM employees where emp_id = %s order by emp_id DESC", (id,))
+
+            return cursor.fetchall()
+        except:
+            return ()
+        finally:
+            con.close()
+
+    def get_deparments_list(self):
+        con = Database.connect(self)
+        cursor = con.cursor()
+        try:
+            cursor.execute("SELECT * FROM deparments")
+            return cursor.fetchall()
+        except:
+            return ()
+        finally:
+            con.close()
+
+    def get_all_employee_details_by_id(self, id):
+        con = Database.connect(self)
+        cursor = con.cursor()
+        try:
+            sql ="SELECT * FROM employees AS e JOIN salaries AS s ON e.emp_id = s.emp_id" \
+                 " JOIN dept_emp as de ON e.emp_id = de.emp_no" \
+                 " JOIN deparments as d ON de.dept_no= d.dep_no WHERE e.emp_id = %s"
+            cursor.execute(sql, (id,))
+            return cursor.fetchall()
+        except:
+            return ()
+        finally:
+            con.close()
+
+
